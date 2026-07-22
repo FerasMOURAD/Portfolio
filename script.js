58ab9873b78877b3b848520f31fad65c2d42a39a
feras.mourad.etu <feras.mourad.etu@univ-lille.fr>
2026-07-23 00:59:23 +0200

    Ajout certification Power BI UDEMY

--- script.js
+++ script.js
@@ -136,7 +136,7 @@ function afficherCV(langue) {
                     <p>Arabe (natif)<br>Français (B2)<br>Anglais (C1 – TOEIC 920)<br>Allemand (A2)</p>
 
                     <h4 class="text-warning">Certifications</h4>
-                    <p>PIX Niveau Avancé (2025)<br>Teens Who Code (2018)</p>
+                    <p>Certification Power BI (UDEMY)<br>PIX Niveau Avancé (2025)<br>Teens Who Code (2018)</p>
 
                     <h4 class="text-danger">Centres d'intérêt</h4>
                     <p>Apprentissage autonome sur UDEMY<br>Football<br>Lecture de mangas<br>Séries / Films</p>
@@ -231,7 +231,7 @@ function afficherCV(langue) {
                     <p>Arabic (Native)<br>French (B2)<br>English (C1 – TOEIC 920)<br>German (A2)</p>
 
                     <h4 class="text-warning">Certifications</h4>
-                    <p>PIX Advanced Level (2025)<br>Teens Who Code (2018)</p>
+                    <p>Power BI Certification (UDEMY)<br>PIX Advanced Level (2025)<br>Teens Who Code (2018)</p>
 
                     <h4 class="text-danger">Interests</h4>
                     <p>Self-learning on UDEMY<br>Football<br>Manga reading<br>Series / Movies</p>
