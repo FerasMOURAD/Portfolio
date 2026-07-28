0968b62908b6c22d409adbbe9ae892ccabbe74ab
feras.mourad.etu <feras.mourad.etu@univ-lille.fr>
2026-07-28 16:24:40 +0200

    correction mineure d'orthographe

--- script.js
+++ script.js
@@ -55,8 +55,8 @@ function afficherCV(langue) {
                         <h5>Stagiaire Recherche</h5>
                         <span class="text-muted">CRIStAL, équipe CFHP – Université de Lille | 06/2026 – Présent</span>
                         <ul>
-                            <li>Compare 4 méthodes d'estimation de paramètres sur systèmes dynamiques, garantissant la stabilité numérique via expérimentations Python reproductibles.</li>
-                            <li>Optimise un package open source (refactoring, tests unitaires), améliorant la maintenabilité et la vitesse de traitement des algorithmes.</li>
+                            <li>Comparé 4 méthodes d'estimation de paramètres sur systèmes dynamiques, garantissant la stabilité numérique via expérimentations Python reproductibles.</li>
+                            <li>Optimisé un package open source (refactoring, tests unitaires), améliorant la maintenabilité et la vitesse de traitement des algorithmes.</li>
                             <li>Exécuté des campagnes de calcul intensif sur l'infrastructure distribuée Grid5000 pour l'exécution de codes nécessitant des ressources de calcul importantes.</li>
                         </ul>
                     </div>
